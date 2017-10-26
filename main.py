78ea590d337c8f121c8b49437246f28a39e99164
Jacqueline Xu <jackiestuy@gmail.com>
2017-10-26 14:50:41 -0400

    devlog and more edits

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 username = ""
-contrib = []
-uncontrib = []
+contrib = [] #array of story id's the user in session has contributed to
+uncontrib = [] #array of story id's the user in session has not contributed to
 
 #==========================================================
 
@@ -56,7 +56,7 @@ def home():
 #discover: goes to discover.html
 @my_app.route('/discover')
 def discover():
-    return render_template("home.html", u = uncontrib)
+    return render_template("discover.html", u = uncontrib)
 
 #new: goes to new.html
 @my_app.route('/new')
@@ -68,6 +68,27 @@ def new():
 def edit():
     
 
+#story: goes to storypage.html of requested story
+@my_app.route('/story')
+def story():
+    story_name = "something" #FIX request.form['story']
+    ppl_cont = [] #array of people who contributed to this story
+    story_sec = [] #array of sections in the story
+    story_id = 0 #FIX story_id to equal the story id of story
+    contributors = c.execute("SELECT user FROM edit WHERE id = story_id;") #pulls all story ids that user has contributed to
+    for each in contributors:
+        #add each contributor to a list of contributors
+        ppl_cont.append(each[0])
+    sections = c.execute("SELECT content FROM edit WHERE id = story_id;") #pulls all story ids that user has contributed to
+    for each in sections:
+        #add each section to a list of all sections
+        story_sec.append(each[0])
+    if story_id in contrib:
+        return render_template("storypage.html", title = story_name, contribs = ppl_cont, sections = story_sec)
+    else:
+        return redirect(url_for('edit')) #FIX make sure that this still gets the title?
+    
+    
 #logout: removes session and redirects to root route
 @my_app.route('/logout')
 def logout():
