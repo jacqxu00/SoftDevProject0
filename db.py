c102daff46a8b864589f748c1976af4a4b1de814
Jacqueline Xu <jackiestuy@gmail.com>
2017-10-30 14:16:31 -0400

    added users

--- db.py
+++ db.py
@@ -10,10 +10,13 @@ c = db.cursor()    #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
-c.execute("CREATE TABLE users (user TEXT PRIMARY KEY, pass TEXT)")
-c.execute("CREATE TABLE edit (id INT, user TEXT, section INT, content TEXT)")
-c.execute("CREATE TABLE stories (id INT PRIMARY KEY, title TEXT, numsections INT)")
+c.execute("CREATE TABLE users (user TEXT PRIMARY KEY, pass TEXT);")
+c.execute("CREATE TABLE edit (id INT, user TEXT, section INT, content TEXT);")
+c.execute("CREATE TABLE stories (id INT PRIMARY KEY, title TEXT, numsections INT);")
 
+c.execute("INSERT INTO users VALUES('bananas','crazy');")
+c.execute("INSERT INTO users VALUES('jackie','pass');")
+c.execute("INSERT INTO users VALUES('potato','mashed');")
 #==========================================================
 db.commit() #save changes
 db.close()  #close database
